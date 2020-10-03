4ec6c1c4ef847364901eb5a49dfea0b4bcbd999b
Horus_Vidal <vidal.m.horus@gmail.com>
2020-10-03 01:43:09 -0500

    Player moves according to player input - WASD to move - V modifier moves slowly Player can also draw line - Hold B to draw until the line limit runs out, release to do nothing for now

--- src/entity/Player/Player.cpp
+++ src/entity/Player/Player.cpp
@@ -1,1 +1,63 @@
 #include "Player.hpp"
+
+void Player::update(Point move, bool shifted)
+{
+    // Set movement based on vector
+    movement = shifted ? move * i_PlayerSpd * 0.5f : move * i_PlayerSpd;
+
+    // Keep player in screen
+    Point next_p = get_center() + movement;
+    if (next_p.get_x() < 16)
+    {
+        set_center(Point(16, next_p.get_y()));
+        movement.set_x(0);
+    }
+    else if (next_p.get_x() > 1350)
+    {
+        set_center(Point(1350, next_p.get_y()));
+        movement.set_x(0);
+    }
+
+    if (next_p.get_y() < 16)
+    {
+        set_center(Point(next_p.get_x(), 16));
+        movement.set_y(0);
+    }
+    else if (next_p.get_y() > 752)
+    {
+        set_center(Point(next_p.get_x(), 752));
+        movement.set_y(0);
+    }
+
+    // Move and reset movement vector
+    Player::move();
+    movement = Point(0,0);
+
+    // Add points if looping
+    if (b_isLooping)
+    {
+        if (f_LineLeft > 0 && get_center() != v_Line.back())
+        {
+            v_Line.push_back(get_center());
+            f_LineLeft -= 0.01f;
+        }
+    }
+}
+
+void Player::start_loop()
+{
+    b_isLooping = true;
+
+    f_LineLeft = f_LineLimit;
+    v_Line.clear();
+    v_Line.push_back(get_center());
+}
+
+void Player::validate_loop() 
+{
+    b_isLooping = false;
+
+    // do_math
+
+    v_Line.clear();
+}
--- src/entity/Player/Player.hpp
+++ src/entity/Player/Player.hpp
@@ -1,21 +1,33 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
-#include "../Entity.hpp"
+#include "../Circular_Entity.hpp"
 
-class Player : public Entity
+class Player : public Circular_Entity
 {
     private:
-        int i_PlayerSpd;
+        float i_PlayerSpd = 10.f;
 
         std::vector<Point> v_Line;
-        float f_LineLimit;
+        float f_LineLimit = 1.0f;
+        float f_LineLeft = f_LineLimit;
+        bool b_isLooping = false;
     public:
-        Player() {};
-        int get_PlayerSpd() { return i_PlayerSpd; };
+        Player() { animations.push_back(Animation("green_circle")); };
+
+        float get_PlayerSpd() { return i_PlayerSpd; };
         void set_PlayerSpd(int i) { i_PlayerSpd = i; };
+        std::vector<Point> get_Line() { return v_Line; };
         float get_LineLimit() { return f_LineLimit; };
         void set_LineLimit(float f) { f_LineLimit = f; };
+        float get_LineLeft() { return f_LineLeft; };
+        void set_LineLeft(float f) { f_LineLeft = f; };
+        bool get_isLooping() { return b_isLooping; };
+        void set_isLooping(bool b) { b_isLooping = b; };
+
+        void update(Point move, bool shifted);
+        void start_loop();
+        void validate_loop();
 };
 
 #endif
