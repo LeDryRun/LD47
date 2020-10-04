3132e79efa1d62b53763e2fdb0bf741b1941e88d
Biohazard335 <stupidmonkey67@gmail.com>
2020-10-04 10:18:54 -0500

    made functional bullet types

--- src/entity/Bullet/Homing_Bullet.hpp
+++ src/entity/Bullet/Homing_Bullet.hpp
@@ -4,15 +4,18 @@
 #include "Bullet.hpp" 
 #include "Bullet_Blueprint.hpp" 
 
+class Player;
+
 class Homing_Bullet: public Bullet{
 private:
+	Player* player=nullptr;
 public:
 	Homing_Bullet();
 
 	void update()override;
 	void create(Point center_p, int radius_p, Point direction_p)override;
-	Homing_Bullet create_copy(Point center_p, int radius_p, Point direction_p, Enemy* sender);
-	Homing_Bullet create_copy(Bullet_Blueprint);
+	Homing_Bullet create_copy(Point center_p, int radius_p, Point direction_p, Enemy* sender,Player* player);
+	Homing_Bullet create_copy(Bullet_Blueprint,Player* player);
 
 };
 
--- src/gamestate/states/Test/Daniel_Test_State.hpp
+++ src/gamestate/states/Test/Daniel_Test_State.hpp
@@ -2,12 +2,9 @@
 #define DTEST_STATE_HPP
 
 #include "../../Gamestate.hpp"
-#include "../../../tilemap/Tilemap.hpp"
 #include "../../../audio/Audiohandler.hpp"
-#include "../../../entity/Circle_Player.hpp"
 #include "../../../entity/Bullet/Bullet_Manager.hpp"
-#include "../../../entity/enemy/Enemy_Straight.hpp"
-#include "../../../entity/Bullet/Linear_Bullet.hpp"
+#include "../../../wave/Wave_Manager.hpp"
 #include "../../../communal/Tick_Timer.hpp"
 
 class Daniel_Test_State:public Gamestate{
@@ -15,12 +12,18 @@ private:
 
 	Layer test_layer;
 
-	Circle_Player player;
-	Circle_Player other;
-
 	Enemy_Straight dummy_enemy;
 
 	Bullet_Manager bullet_manager;
+	Wave_Manager wave_manager;
+
+	Wave wave_one;
+	Wave wave_two;
+
+	Player player = Player(&bullet_manager);
+
+    World_Data world;
+
 	Tick_Timer enemy_spawn_timer;
 
 
@@ -35,7 +38,10 @@ public:
 	void check_keyboard(Keyblade&);
 	void check_gamepad(Gamepad&);
 
+	void create_waves();
+	void update_player(Mousey&,Keyblade&,Gamepad&);
 
+	void add_bullets();
 };
 
 
